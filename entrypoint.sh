38e2be0b66b78c944a12ebfc787ddbb6f61a1720
EbolaWare <averittexpress@gmail.com>
2020-03-03 18:05:26 -0600

    cleaned up entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,16 @@
 help='You have several options when running this container:\n
 USAGE: docker run --rm sigma [command]\n
        docker run --rm -it sigma bash (gives bash execution environment)\n\n
-
 [commands]\n
 help	\tgives this message\n
 tools	\tlists tools made by Neo23x0 & friends\n
 contrib	\tlists tools made by the community, but trusted by developers\n
 sigmac	\tconvert sigma rules to queries for... well lots of stuff.\n\n
 I recommend you visit the github of the maintainer.\n
-https://github.com/Neo23x0/sigma\n
-'
-
+https://github.com/Neo23x0/sigma\n'
 case $1 in 
 	help) echo -e $help ;;
-	tools) ls tools/ ;;
-	contrib) ls contrib/ ;;
+	tools) ls /opt/sigma/tools/ ;;
+	contrib) ls /opt/sigma/contrib/ ;;
 	sigmac) tools/$* ;;
 esac
-
